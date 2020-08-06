ac133a8245e38ab7310ec3ad92c5465260031a69
Takudzwa413 <takuman456@gmail.com>
2020-08-07 00:01:03 +0200

    updated

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && 'nav_black'}`}>
+      <img
+        className='nav_logo'
+        src='https://pmcvariety.files.wordpress.com/2020/05/netflix-logo.png?w=600'
+        alt='Netflix Logo'
+      />
+      <img
+        className='nav_avatar'
+        src='https://i.pinimg.com/236x/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1--horse-stuff-sleepover.jpg'
+        alt='Netflix_Logo'
+      />
+    </div>
+  );
+}
+
+export default Nav;
